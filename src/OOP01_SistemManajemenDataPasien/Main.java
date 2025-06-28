2061d2ea45dd1adf295e163056d525977f415abb
Delano <del.191006@gmail.com>
2025-06-28 21:02:34 +0700

    [Project 03 : Managemen Data Pasien] Fitur pencarian data berdasarkan nama complete

--- src/OOP01_SistemManajemenDataPasien/Main.java
+++ src/OOP01_SistemManajemenDataPasien/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+
 		
 		System.out.println("\n\n===== Input Data Pasien =====\n");
 		System.out.println("Masukan Jumlah Pasien: ");
@@ -34,15 +35,59 @@ public class Main {
 			
 		}
 		
-		System.out.println("\n\n===== Data Pasien Klinik =====\n");
-		for(int i = 0; i < dataPasien.length; i++) {
-			System.out.println("Data Pasien ke-" + (i+1));
-			dataPasien[i].tampilkanInfo();
+		int userInput;
+		do {
+		System.out.println("\n============== 🏥 SISTEM MANAGEMEN PASIEN ==============\n\n");
+		System.out.println("1. 👪 Tampilkan Data Pasien \n"
+						 + "2. 🔎 Cari Pasien\n"
+						 + "9. ⬅ Keluar");
+		
+		System.out.println("\nMasukan Angka Menu: ");
+		userInput = input.nextInt();
+		
+		
+		
+		switch(userInput) {
+			case 1 : 
+				
+				System.out.println("\n\n===== Data Pasien Klinik =====\n");
+				for(int i = 0; i < dataPasien.length; i++) {
+					System.out.println("Data Pasien ke-" + (i+1));
+					dataPasien[i].tampilkanInfo();
+				}
+				
+				System.out.println("Total Pasien yang tercatat: " + dataPasien.length);
+			
+				break;
+			
+			case 2 : 
+				input.nextLine();
+				boolean ditemukan = false;
+				System.out.println("n\n===== Cari Pasien Klinik =====\n");
+				
+				System.out.println("Masukan nama Pasien yang ingin dicari: ");
+				String namaPasien = input.nextLine();
+				
+				for(int i = 0; i < dataPasien.length; i++) {
+					if(dataPasien[i] != null && dataPasien[i].nama.equalsIgnoreCase(namaPasien)) {
+						ditemukan = true;
+						dataPasien[i].tampilkanInfo();
+						break;
+					} 
+				}
+
+				if(!ditemukan){
+					System.out.println("Pasien dengan nama '" + namaPasien + "' tidak ditemukan dalam data.");
+				}
+				
+				break;
+			
+			default : 
+				System.out.println("Program Berakhir.Terima Kasih!");
 		}
 		
 		
-		System.out.println("Total Pasien yang tercatat: " + dataPasien.length);
-		
+		}while(userInput != 9);
 	}
 
 }
