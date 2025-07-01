24ac405505aee254cf3f6145c4ed053ec701a60b
Delano <del.191006@gmail.com>
2025-07-01 21:02:56 +0700

    [Project 04 : Sistem Reservasi Kamar Hotel] Fixed minor features & Implementing array to System.

--- src/OOP02_SistemReservasiKamarHotel/Main.java
+++ src/OOP02_SistemReservasiKamarHotel/Main.java
@@ -4,18 +4,42 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Array dependency dari Class Reservasi
+		Reservasi[] daftarReservasi = new Reservasi[10];
+		int counter = 0;
+		
 		StandardRoom room1 = new StandardRoom(101, 300000, "Standard Room");
 		DeluxeRoom	room2 = new DeluxeRoom(201, 500000, "Deluxe Room");
 		SuiteRoom room3 = new SuiteRoom(301, 750000, "Suite Room");
 		
 		Pelanggan p1 = new Pelanggan("Delano Bariq","0821-6937-2548");
+		Pelanggan p2 = new Pelanggan("Andi Prasetyo","0812-3456-7890");
+		Pelanggan p3 = new Pelanggan("Hanah Smith","0899-9991-1119");
 		
 		Reservasi r1 = new Reservasi(p1,room1,3);
-		System.out.println("🏨 Daftar Reservasi Hotal\n");
-		r1.tampilkanDetail();
+		Reservasi r2 = new Reservasi(p2,room2,2);
+		Reservasi r3 = new Reservasi(p3,room3,5);
 		
 		
+		//Tambah data ke array
+		daftarReservasi[counter] = r1;
+		counter++;
 		
+		daftarReservasi[counter] = r2;
+		counter++;
+		
+		daftarReservasi[counter] = r3;
+		counter++;
+		
+		//OUTPUT
+		System.out.println("🏨 Daftar Reservasi Hotel\n\n");
+		System.out.println("===================================");
+		
+		for(int i = 0; i < counter; i++) {
+			daftarReservasi[i].tampilkanDetail();
+			System.out.println("===================================");
+
+		}
 		
 	}
 
